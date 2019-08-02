e164f6c9e539bd9f85c6c3f40f7d9d8414a64909
tongpisztt <pisztt@gmail.com>
2019-08-02 22:55:57 +0700

    ลบ โฟลเดอร์ซ้ำ

--- laraveltutorial/basiclaravel/resources/views/edit.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('master')
-@section('title','EDIT')
-@section('content')
-    <h1>EDIT PAGE</h1>
-@stop
--- laraveltutorial/basiclaravel/resources/views/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('master')
-@section('title','HOME')
-@section('content')
-    <h1>HOME PAGE</h1>
-@stop
